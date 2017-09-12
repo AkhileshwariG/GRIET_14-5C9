f05e0129d9393b18bee2c8f63f5e25fc5d81cd16
AkhileshwariG <31853555+AkhileshwariG@users.noreply.github.com>
2017-09-12 12:43:19 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+import Student.*;
+import Student.Array.Operation.*;
+import StudentGroup.*;
+
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		Student s1,s2,s3=new Student;
+		s2=s1.StudentArrayOperation;
+		s3=s1.StudentArray;
+		
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
