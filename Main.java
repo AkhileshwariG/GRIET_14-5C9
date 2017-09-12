c13e237a25508cd24c3d5335a9e8cea96441a466
AkhileshwariG <31853555+AkhileshwariG@users.noreply.github.com>
2017-09-12 13:06:41 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,22 +1,35 @@
 import java.util.*;
 import java.io.*;
+import java.bufferReader.*;
 import Student.*;
 import Student.Array.Operation.*;
 import StudentGroup.*;
 
 
 public class Main {
-
+	String student;
+	BufferReader br= new BufferReader(args[0],",");
 	public static void main(String[] args) 
 	{
-		Student s1,s2,s3=new Student;
-		s2=s1.StudentArrayOperation;
-		s3=s1.StudentArray;
+		Student s1=new Student[100];
 		
-		
-		//You may test that your code works find here
-		//Please check that your code works and has no 
-		//compilation problems before to submit
+		s1.setStudent( s, 1);
+	s1.add( student, 2);
+	s1.addFirst( student);
+	s1.addLast( student);
+	s1.remove(2);
+	s1.remove(student);
+	s1.removeFromIndex(2);
+	s1.removeFromElement(s);
+	s1.removeToIndex(2);
+	s1.removeToElement(student);
+	s1.getByBirthDate(28-10-1996);
+	s1.getBetweenBirthDates(10-11-1989, 28-10-1996);
+	s1.getNearBirthDate(28-10-1996,14);
+	s1.getCurrentAgeByDate(12);
+	s1. getStudentsByAge(20);
+	s1.getStudentsWithMaxAvgMark();
+
 	}
 
 }
